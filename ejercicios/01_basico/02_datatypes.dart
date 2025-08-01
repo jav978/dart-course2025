7f3ea525d9df0cbaab293ca195e329a4b1ce4620
José Antonio Vásquez <jvasquez978@gmail.com>
2025-08-01 17:25:53 -0400

    02_datatypes.dart

---
+++ ejercicios/01_basico/02_datatypes.dart
@@ -0,0 +1,18 @@
+// ======= Punto de entrada =======
+main() {
+  //======= Declaracion de variables ========
+  //=== Tipos de datos Numeros ===
+
+  // var a = 10; no recomendable
+  int a = 10;
+  double b = 10.5;
+
+  int? c = null; //Null safety
+
+  int _a = 60;
+  double $b = 70.5;
+
+  double resultado = _a + $b;
+
+  print(resultado);
+}
