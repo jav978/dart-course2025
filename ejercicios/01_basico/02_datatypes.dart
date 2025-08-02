3decbcdc7a235d59ea9d310d447c3b55b1583894
José Antonio Vásquez <jvasquez978@gmail.com>
2025-08-01 21:29:09 -0400

    02_datatypes.dart/map

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -14,5 +14,101 @@ main() {
 
   double resultado = _a + $b;
 
-  print(resultado);
+  // print(resultado);
+
+  //=== Tipos de datos String ===
+
+  // var nombre = "Juan"; // No recomendado
+  String nombre = "Tony";
+  String nombre2 = 'Tony';
+
+  //String nombre3 ='O\'connor';
+
+  String nombre3 = "O'connor";
+
+  String apellido = "Stark";
+
+  String nombrecompleto = '$nombre $apellido'.toUpperCase();
+
+  String multilinea = ''' 
+  hola mundo 
+  ¿Cómo estás?
+  ¿Dónde vives?
+  $nombrecompleto
+  $nombre2
+  'Oconnor''';
+
+  //print(multilinea);
+
+  //=== Tipos de datos Booleanos ===
+
+  // var isActive; no recomendado
+  bool isActive = true;
+
+  bool isNotActive = !isActive;
+
+  //print(isNotActive);
+
+  //=== Tipos de datos Listas ===
+
+  //
+
+  List<String> villanos = ['Lex', 'Red Skull', 'Doom']; // son base 0 o index 0
+
+  //villanos[0] = 'Thanos';
+  villanos.add('Thanos');
+  villanos.add('Thanos');
+  villanos.add('Thanos');
+  villanos.add('Thanos');
+
+
+  var villanosSet = villanos.toSet();
+
+  print(villanosSet.toList());
+
+  print(villanos);
+
+  //=== Tipos de datos Sets ===
+
+ // var villanos2 = {'Lex', 'Red Skull', 'Doom'};
+  Set<String> villanos2 = {'Lex', 'Red Skull', 'Doom'};
+
+  villanos2.add('Thanos');
+
+  print(villanos2);
+
+
+  //=== Tipos de datos Maps ===
+
+  Map<String, int> villanos3 = {'Lex': 1, 'Red Skull': 2, 'Doom': 3}; 
+
+  villanos3['Thanos'] = 4;
+
+  print(villanos3);
+
+  Map<String, dynamic> ironman = {
+    'name': 'Tony Stark',
+    'power': 'Inteligencia',
+    'age': 50,
+    'isAlive': true,
+  };
+
+
+
+  Map<int, dynamic> thor = {
+    1: 'Tony Stark',
+    2: 'Inteligencia',
+    3: 50,
+    4: true,
+  };
+
+  print(thor);
+
+  print(ironman['name']); 
+  print(ironman['power']); 
+  print(ironman['age']); 
+  print(ironman['isAlive']); 
+
+
+
 }
